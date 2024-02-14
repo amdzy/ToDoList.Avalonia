8f6300ae7d8f546711177ea447f8b7119be18e8f
Soul-Remix <soulremix-dev@pm.me>
2024-02-14 20:46:52 +0300

    Add ToDo view

---
+++ Views/ToDoListView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ToDoList.Avalonia.Views;
+
+public partial class ToDoListView : UserControl
+{
+    public ToDoListView()
+    {
+        InitializeComponent();
+    }
+}
