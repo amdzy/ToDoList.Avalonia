3b2ca0a8bc35c387ca23c639fe9a07a7f04c81ea
Soul-Remix <soulremix-dev@pm.me>
2024-02-15 14:46:52 +0300

    Bind ok command and close command

--- ViewModels/AddItemViewModel.cs
+++ ViewModels/AddItemViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace ToDoList.Avalonia.ViewModels;
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace ToDoList.Avalonia.ViewModels;
 
 public class AddItemViewModel : ViewModelBase
 {
-    public string Description { get; set; } = string.Empty;
+    public string _description = string.Empty;
+
+    public ReactiveCommand<Unit, ToDoItem> OkCommand { get; }
+    public ReactiveCommand<Unit, Unit> CloseCommand { get; }
+
+    public AddItemViewModel()
+    {
+        var isValidObservable = this.WhenAnyValue(x => x.Description, x => !string.IsNullOrEmpty(x));
+
+        OkCommand = ReactiveCommand.Create(() => new ToDoItem { Description = Description }, isValidObservable);
+        CloseCommand = ReactiveCommand.Create(() => { });
+    }
+
+    public string Description { get => _description; set => this.RaiseAndSetIfChanged(ref _description, value); }
 }
