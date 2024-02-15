e42af6d875c0909292de34329a5952da4b663ec8
Soul-Remix <soulremix-dev@pm.me>
2024-02-15 13:21:21 +0300

    Implement to do list service

---
+++ Services/ToDoListService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TodoList.Avalonia;
+
+public class ToDoListService
+{
+    public IEnumerable<ToDoItem> GetItems()
+    {
+        return [
+            new ToDoItem{Description = "Walk the dog"},
+            new ToDoItem{Description = "Buy some milk"},
+            new ToDoItem{Description = "Learn Avalonia",IsChecked = true}
+        ];
+    }
+}
