0a3537b14ffbd700e501a7cb824a5e0adbbd1f4b
Soul-Remix <soulremix-dev@pm.me>
2024-02-15 13:34:51 +0300

    Implement to do list view model

---
+++ ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using TodoList.Avalonia.ViewModels;
+
+namespace TodoList.Avalonia;
+
+public class ToDoListViewModel : ViewModelBase
+{
+    public ToDoListViewModel(IEnumerable<ToDoItem> items)
+    {
+        ListItems = new ObservableCollection<ToDoItem>(items);
+    }
+
+    public ObservableCollection<ToDoItem> ListItems { get; }
+}
