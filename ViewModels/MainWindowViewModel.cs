cc53145a0edc7d75a814a5551610877ea4ec5802
Soul-Remix <soulremix-dev@pm.me>
2024-02-15 14:46:16 +0300

    Add navigation to add item view

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,27 @@
-﻿namespace ToDoList.Avalonia.ViewModels;
+﻿using ReactiveUI;
+
+namespace ToDoList.Avalonia.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private ViewModelBase _contentViewModel;
     public MainWindowViewModel()
     {
         var service = new ToDoListService();
         ToDoList = new ToDoListViewModel(service.GetItems());
+        _contentViewModel = ToDoList;
     }
 
     public ToDoListViewModel ToDoList { get; }
+
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public void AddItem()
+    {
+        ContentViewModel = new AddItemViewModel();
+    }
 }
